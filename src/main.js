d97f15a856e4c2fdc137b099a8780ed0926e698a
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-01-05 13:38:30 +0700

    Use mySQL with NodeJS

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const ViewEngineConfig = require("./config/view.engine");
 const webRoutes = require("./routes/web");
+const mySQL = require("mysql2");
 
 const app = express();
 const port = process.env.PORT;
@@ -10,6 +11,22 @@ const hostname = process.env.HOST_NAME;
 ViewEngineConfig(app);
 
 app.use('/',webRoutes);
+
+const connection = mySQL.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+connection.query(
+    `SELECT * FROM users`,
+    function (err, results,fields) {
+        console.log("Check results",results);
+        console.log("Check fields", fields);
+    }
+)
 
 app.listen(port, hostname,() => {
     console.log(`Server running at http://${hostname}:${port}/`);
