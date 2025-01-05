a6c75caef7e54ea26ad14af285ef37a804cde61f
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-01-05 12:49:11 +0700

    MVC with NodeJS Part-1

--- src/main.js
+++ src/main.js
@@ -1,23 +1,16 @@
-const express = require('express');
-const path = require("node:path");
 require('dotenv').config();
+const express = require('express');
+const ViewEngineConfig = require("./config/view-engine");
+const webRoutes = require("./routes/web");
 
 const app = express();
 const port = process.env.PORT;
 const hostname = process.env.HOST_NAME;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+ViewEngineConfig(app);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
-
-app.get('/kirito', (req, res) => {
-    res.render('sample');
-})
+app.use('/',webRoutes);
 
 app.listen(port, hostname,() => {
     console.log(`Server running at http://${hostname}:${port}/`);
-})
+});
