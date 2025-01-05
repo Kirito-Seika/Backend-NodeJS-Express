3ff762d1f31607a75d64c2c029a53f5ed2ae9cb2
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-01-06 01:15:09 +0700

    Get Customer API with Pagination

--- src/routes/api.js
+++ src/routes/api.js
@@ -36,4 +36,15 @@ routerAPI.put('/customers', UpdateCustomersAPI);
 routerAPI.delete('/customers', DeleteCustomerAPI);
 routerAPI.delete('/many-customers', DeleteManyCustomersAPI);
 
+routerAPI.get('/info', (req, res) => {
+    res.status(200).send({
+        data: req.query
+    })
+});
+routerAPI.get('/info/:name/:address/:phone/:email', (req, res) => {
+    res.status(200).send({
+        data: req.params
+    })
+});
+
 module.exports = routerAPI;
