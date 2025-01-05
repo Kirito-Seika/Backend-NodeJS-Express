4243c0b21f5409d7e583b3d666b446f1e70f39b4
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-01-05 11:58:13 +0700

    Environment Variables

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require("node:path");
+require('dotenv').config();
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
+const hostname = process.env.HOST_NAME;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -15,6 +17,6 @@ app.get('/kirito', (req, res) => {
     res.render('sample');
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(port, hostname,() => {
+    console.log(`Server running at http://${hostname}:${port}`)
 })
