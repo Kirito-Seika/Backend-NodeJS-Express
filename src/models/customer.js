0131ae1aff3d0e9c0c5fcc4826f5cc63727da7d9
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-01-05 20:03:21 +0700

    Models Customer

---
+++ src/models/customer.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const customerSchema = new mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        address: String,
+        phone: Number,
+        email: String,
+        image: {type: String},
+        description: String,
+    },
+    {
+        timestamps: true
+    }
+);
+const Customer = mongoose.model('customer', customerSchema);
+module.exports = Customer;
